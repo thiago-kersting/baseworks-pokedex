7b794c722273ba91dfa821f2a346c33adec4935d
Thiago Kersting Puls <thiagokpuls@gmail.com>
2024-10-02 07:27:13 -0300

    :heavy_plus_sign: add coverage test

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
     plugins: [vue()],
     test: {
         globals: true,
-        environment: 'happy-dom'
+        environment: 'happy-dom',
+        coverage: {
+            provider: 'istanbul',
+            reporter: ['text', 'json', 'html'],
+        },
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
     }
 })
